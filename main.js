e27760037adaf0ab3bc623fa8bbab5db691adf8e
Guilherme23645 <gui.depaula236@gmail.com>
2025-06-26 11:12:53 -0300

    updates

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+window.onload = function() {
+    const div1 = document.getElementById("div1")
+    const div2 = document.getElementById("div2")
+    const btn = document.getElementById("btn")
+    let changed = true;
+    btn.onclick = () => {
+        changed = !changed
+        div1.style.width = changed ? "calc(100% / 3)" : "calc(100% / 5)"
+        div2.style.width = changed ? "calc(2 * 100% / 3)" : "calc(4 * 100% / 5)"
+    };
+}
